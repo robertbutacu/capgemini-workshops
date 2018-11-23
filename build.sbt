45fa6fc8d53a8c9504cf3fbdaac8263842e8af2e
robertbutacu <robertbutacu@gmail.com>
2018-11-23 09:58:45 +0000

    added project and first workshop

---
+++ build.sbt
@@ -0,0 +1,8 @@
+name := "capgemini-workshops"
+
+version := "0.1"
+
+scalaVersion := "2.12.7"
+
+libraryDependencies += "org.typelevel" %% "cats-core" % "1.5.0-RC1"
+libraryDependencies += "org.typelevel" %% "cats-effect" % "1.0.0"
---
+++ src/main/scala/typeclasses/Typeclass.scala
@@ -0,0 +1,14 @@
+package typeclasses
+
+object Typeclass {
+
+  //1. Define a typeclass  for an Addable type
+
+  //2. Implement a few implicits for various types
+
+  //3. Write a function which takes generics parameters and an implicit Addable - use it to add 2 Addables
+
+  //4. Write an implicit class for the Addable instances
+
+  //5. Rewrite the function using the implicit class
+}
